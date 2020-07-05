d5c23618e672f9903bdd5bf8b5b1dea7c2ec1e59
yutashimizu0612 <s08388yskh@gmail.com>
2020-07-05 11:14:19 +0900

    お客様情報入力の見た目

--- src/components/InputCustomerInfo.js
+++ src/components/InputCustomerInfo.js
@@ -2,12 +2,52 @@ import React from 'react';
 
 function InputCustomerInfo() {
   return (
-    <div class="card">
-      <header class="card-header">
-        <p class="card-header-title">InputCustomerInfo</p>
-      </header>
-      <div class="card-content">
-        <div class="content"></div>
+    <div className="form-box">
+      <div className="form-box__header">
+        <span className="tag is-info form-box__tag">STEP1</span>
+        <p className="is-size-5 has-text-centered">
+          お客様の情報を入力してください。
+        </p>
+      </div>
+      <div className="form-box__content">
+        <dl>
+          <dt>性別</dt>
+          <dd>
+            <input type="radio" value="male" name="gender" id="male" />
+            <label htmlFor="male" className="mr-3">
+              男性
+            </label>
+            <input type="radio" value="female" name="gender" id="female" />
+            <label htmlFor="female">女性</label>
+          </dd>
+          <dt className="mt-5">生年月日</dt>
+          <dd>
+            <div className="is-inline-flex">
+              <div className="select">
+                <select>
+                  <option>1990年（平成2）</option>
+                </select>
+              </div>
+              <span className="mt-2 ml-2 mr-4 has-text-justified">年</span>
+            </div>
+            <div className="is-inline-flex">
+              <div className="select">
+                <select>
+                  <option>1</option>
+                </select>
+              </div>
+              <span className="mt-2 ml-2 mr-4 has-text-justified">月</span>
+            </div>
+            <div className="is-inline-flex">
+              <div className="select">
+                <select>
+                  <option>1</option>
+                </select>
+              </div>
+              <span className="mt-2 ml-2 mr-4 has-text-justified">日</span>
+            </div>
+          </dd>
+        </dl>
       </div>
     </div>
   );
