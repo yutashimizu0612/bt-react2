b98169b6b6573b57bb699bbdcb908ada94d5e113
yutashimizu0612 <s08388yskh@gmail.com>
2020-07-10 08:53:03 +0900

    質問ページのhtml構造変更

--- src/pages/Answer.js
+++ src/pages/Answer.js
@@ -10,39 +10,39 @@ function Answer() {
           <p className="is-size-5 has-text-centered">以下にお答えください</p>
         </div>
         <div className="form-box__content">
-          <dl>
-            <dt>現在、生命保険に加入されていますか？</dt>
-            <dd>
+          <ul>
+            <li className="question-item">
+              <p>現在、生命保険に加入されていますか？</p>
               <input type="radio" name="question1" id="question1Yes" />
               <label htmlFor="question1Yes" className="mr-3">
                 はい
               </label>
               <input type="radio" name="question1" id="question1No" />
               <label htmlFor="question1No">いいえ</label>
-            </dd>
-            <dt className="mt-6">
-              現在入院中ですか。または、最近3ヶ月以内に医師の診察・検査の結果、入院・手術をすすめられたことはありますか？
-            </dt>
-            <dd>
+            </li>
+            <li className="question-item">
+              <p className="mt-6">
+                現在入院中ですか。または、最近3ヶ月以内に医師の診察・検査の結果、入院・手術をすすめられたことはありますか？
+              </p>
               <input type="radio" name="healthState" id="question2Yes" />
               <label htmlFor="question2Yes" className="mr-3">
                 はい
               </label>
               <input type="radio" name="healthState" id="question2No" />
               <label htmlFor="question2No">いいえ</label>
-            </dd>
-            <dt className="mt-6">
-              過去5年以内に、病気やけがで、手術をうけたことまたは継続して7日以上の入院をしたことがありますか？
-            </dt>
-            <dd>
+            </li>
+            <li className="question-item">
+              <p className="mt-6">
+                過去5年以内に、病気やけがで、手術をうけたことまたは継続して7日以上の入院をしたことがありますか？
+              </p>
               <input type="radio" name="hospitalization" id="question3Yes" />
               <label htmlFor="question3Yes" className="mr-3">
                 はい
               </label>
               <input type="radio" name="hospitalization" id="question3No" />
               <label htmlFor="question3No">いいえ</label>
-            </dd>
-          </dl>
+            </li>
+          </ul>
         </div>
       </div>
       <div className="buttons is-centered mt-5">
